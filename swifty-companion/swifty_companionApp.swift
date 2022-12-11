3aa492e9b2380c1adf47c197bbede35d9fbd39fc
IKyllian <kyllian.delporte@gmail.com>
2022-12-11 11:53:10 +0100

    Initial Commit

---
+++ swifty-companion/swifty_companionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swifty_companionApp.swift
+//  swifty-companion
+//
+//  Created by Kyllian on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct swifty_companionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
